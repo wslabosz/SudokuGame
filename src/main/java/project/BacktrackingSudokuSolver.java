8bb20905e6bea189f4dc0cfc26c6f53ca3ba7fa8
Wojciech Slabosz <230000@edu.p.lodz.pl>
2020-11-07 17:16:20 +0100

    solver debugging

--- src/main/java/project/BacktrackingSudokuSolver.java
+++ src/main/java/project/BacktrackingSudokuSolver.java
@@ -8,20 +8,21 @@ import java.util.List;
 
 public class BacktrackingSudokuSolver implements SudokuSolver {
 
+    private static final Integer[] numbers = {1, 2, 3, 4, 5, 6, 7, 8, 9};
+    private static final List<Integer> randomizedNumbers = Arrays.asList(numbers);
+
     @Override
     public void solve(SudokuBoard board) {
+        Collections.shuffle(randomizedNumbers);
         solution(board);
     }
 
     private boolean solution(SudokuBoard board) {
-        Integer[] numbers = {1, 2, 3, 4, 5, 6, 7, 8, 9};
-        List<Integer> randomizedNumbers = Arrays.asList(numbers);
         for (int row = 0; row < SIZE; row++) {
             for (int col = 0; col < SIZE; col++) {
                 if (board.getNumberFromPosition(row, col) == 0) {
                     for (int i = 0; i < SIZE; i++) {  //inserting numbers
                         // zadeklarowac tablice mieszac i wpisywac za generowanie
-                        Collections.shuffle(randomizedNumbers);
                         board.setNumber(row, col, randomizedNumbers.get(i));
                         if (board.getRow(row).verify() && board.getColumn(col).verify()
                                 && board.getBox(row, col).verify()) {
