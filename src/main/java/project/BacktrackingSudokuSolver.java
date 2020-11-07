41a389ad4794f7e089843a32d402e52587ac9c1a
Wojciech Slabosz <230000@edu.p.lodz.pl>
2020-11-07 15:44:32 +0100

    changed solution in BacktrackingSudokuSolver

--- src/main/java/project/BacktrackingSudokuSolver.java
+++ src/main/java/project/BacktrackingSudokuSolver.java
@@ -8,45 +8,6 @@ import java.util.List;
 
 public class BacktrackingSudokuSolver implements SudokuSolver {
 
-    private boolean rowEligibility(int row, int number, SudokuBoard board) {
-        for (int i = 0; i < SIZE; i++) {
-            //checking if the plausible number is already in row
-            if (board.getNumberFromPosition(row, i) == number) {
-                return true;
-            }
-        }
-        return false;
-    }
-
-    private boolean colEligibility(int col, int number, SudokuBoard board) {
-        for (int i = 0; i < SIZE; i++) {
-            //checking if the plausible number is already in column
-            if (board.getNumberFromPosition(i, col) == number) {
-                return true;
-            }
-        }
-        return false;
-    }
-
-    private boolean boxEligibility(int row, int col, int number, SudokuBoard board) {
-        int rows = row - row % 3;
-        int columns = col - col % 3;
-        for (int i = rows; i < rows + 3; i++) {
-            for (int j = columns; j < columns + 3; j++) {
-                //checking if the plausible number is already in column
-                if (board.getNumberFromPosition(i, j) == number) {
-                    return true;
-                }
-            }
-        }
-        return false;
-    }
-
-    private boolean sudokuRules(int row, int col, int number, SudokuBoard board) {
-        return !rowEligibility(row, number, board) && !colEligibility(col, number, board)
-                && !boxEligibility(row, col, number, board);
-    }
-
     @Override
     public void solve(SudokuBoard board) {
         for (int i = 0; i < SIZE; i++) {
@@ -66,13 +27,13 @@ public class BacktrackingSudokuSolver implements SudokuSolver {
                         Integer[] numbers = {1, 2, 3, 4, 5, 6, 7, 8, 9};
                         List<Integer> randomizedNumbers = Arrays.asList(numbers);
                         Collections.shuffle(randomizedNumbers);
-                        if (sudokuRules(row, col, randomizedNumbers.get(i), board)) {
-                            board.setNumber(row, col, randomizedNumbers.get(i));
+                        board.setNumber(row, col ,randomizedNumbers.get(i));
+                        if (board.getRow(row).verify() && board.getColumn(col).verify() && board.getBox(row, col).verify()) {
                             if (solution(board)) {
                                 return true;
-                            } else {
-                                board.setNumber(row, col, 0);
                             }
+                        } else {
+                            board.setNumber(row, col, 0);
                         }
                     }
                     return false;
