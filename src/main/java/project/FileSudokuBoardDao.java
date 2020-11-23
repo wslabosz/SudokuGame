2808d5a2af784bd60ba12a508ebed8fe61aa24d9
229951 <229951@edu.p.lodz.pl>
2020-11-23 15:50:11 +0100

    dodano przekazywanie filename do konstruktora klasy FileSudokuBoardDao

--- src/main/java/project/FileSudokuBoardDao.java
+++ src/main/java/project/FileSudokuBoardDao.java
@@ -1,6 +1,15 @@
 package project;
 
+import java.io.File;
+
 public class FileSudokuBoardDao implements Dao {
+
+    File file;
+
+    public FileSudokuBoardDao(String filename) {
+        this.file = new File(filename + ".txt");
+    }
+
     @Override
     public Object read() {
         return null;
