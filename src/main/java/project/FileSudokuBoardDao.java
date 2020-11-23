c8658f220812465274a784117cec4436a6743de6
229951 <229951@edu.p.lodz.pl>
2020-11-23 15:34:45 +0100

    stworzono implementację interfejsu Dao (FileSudokuBoardDao)

---
+++ src/main/java/project/FileSudokuBoardDao.java
@@ -0,0 +1,15 @@
+package project;
+
+public class FileSudokuBoardDao implements Dao {
+    @Override
+    public Object read() {
+        return null;
+    }
+
+    @Override
+    public void write(Object obj) {
+
+    }
+
+    public void finalize() {}
+}
