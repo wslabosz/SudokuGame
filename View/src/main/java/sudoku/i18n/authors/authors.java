b3558c84cc149209378f32facec1ff1db821fdae
229951 <229951@edu.p.lodz.pl>
2020-12-20 15:59:03 +0100

    added listResourceBundle authors

---
+++ View/src/main/java/sudoku/i18n/authors/authors.java
@@ -0,0 +1,10 @@
+package sudoku.i18n.authors;
+
+public class authors extends java.util.ListResourceBundle {
+    @Override
+    protected Object[][] getContents() {
+        return new Object[][] {
+            {"authors", "Authors: Wojciech Slabosz, Michal Malarski"}
+        };
+    }
+}
---
+++ View/src/main/java/sudoku/i18n/authors/authors_pl_PL.java
@@ -0,0 +1,10 @@
+package sudoku.i18n.authors;
+
+public class authors_pl_PL extends java.util.ListResourceBundle {
+    @Override
+    protected Object[][] getContents() {
+        return new Object[][] {
+            {"authors", "Autorzy: Wojciech Słabosz, Michał Malarski"}
+        };
+    }
+}
